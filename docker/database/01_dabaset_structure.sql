f6854b2d41ef5f926cd97d2a9be318b08967f1c8
Hector Ventura <hectorvent@gmail.com>
2018-12-13 14:54:42 -0500

    Fix database table field

--- docker/database/01_dabaset_structure.sql
+++ docker/database/01_dabaset_structure.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `token` (
   `id` char(36) NOT NULL,
   `application_id` char(36) NOT NULL,
   `username` varchar(45) NOT NULL,
-  `code` char(36) NOT NULL,
+  `code` char(32) NOT NULL,
   `lifeend` bigint(11) NOT NULL,
   `created_at` bigint(11) NOT NULL,
   `connected` tinyint(1) NOT NULL DEFAULT 0,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `application` (
   `name` varchar(10) NOT NULL,
   `description` varchar(45) DEFAULT '',
   `enabled` tinyint(4) NOT NULL DEFAULT 1,
-  `api_token` char(36) NOT NULL,
+  `api_token` char(32) NOT NULL,
   `created_at` bigint(11) DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
